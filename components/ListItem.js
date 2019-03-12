a881c6a55bf6922e2f2a20c10b7e5b45c533a29d
alexisraimbault <27raimbault.alexis@gmail.com>
2019-03-12 12:44:49 +0100

    update swipe

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,10 +1,46 @@
 import React, { Component } from 'react';
-import {  View, Text, StyleSheet, TextInput, ScrollView,Image, TouchableOpacity} from 'react-native';
+import {  View, Text, StyleSheet, TextInput, Dimensions, ScrollView,Image, TouchableOpacity, PanResponder, Animated} from 'react-native';
 import { CheckBox } from 'react-native-elements'
 
 
 
 export default class ListItem extends Component {
+  translateX2 = new Animated.Value(Dimensions.get("window").width);
+  translateX = new Animated.Value(0);
+  _panResponder = PanResponder.create({
+    onMoveShouldSetResponderCapture: (evt, gestureState) => {
+      const { dx, dy } = gestureState
+      return dx > 2 || dx < -2 || dy > 2 || dy < -2
+},
+    onMoveShouldSetPanResponderCapture: (evt, gestureState) => {
+      const { dx, dy } = gestureState
+      return dx > 2 || dx < -2 || dy > 2 || dy < -2
+},
+    onPanResponderMove: Animated.event([null, {dx: this.translateX,
+                                              }]),
+    onPanResponderRelease: (e, {vx, dx}) => {
+      const screenWidth = Dimensions.get("window").width;
+      if (vx >= 0.6 || dx >= 0.4 * screenWidth) {
+        this.props.doCheck(this.props.index);
+      }
+      else{
+        console.log(vx);
+        console.log(dx);
+        if (vx <= -0.6 || dx <= -0.4 * screenWidth) {
+          this.props.remove(this.props.index);
+        }
+      }
+      Animated.spring(this.translateX, {
+        toValue: 0,
+        bounciness: 10
+      }).start();
+      Animated.spring(this.translateX2, {
+        toValue: Dimensions.get("window").width,
+        bounciness: 10
+      }).start();
+
+    }
+  });
 
   componentDidMount(){
     if((this.props.focus != -1) &&(!this.props.check[this.props.focus])&&(this.props.focus == this.props.index) ){
@@ -17,7 +53,14 @@ export default class ListItem extends Component {
   render(){
     return this.props.check[this.props.index]
     ?(
-        <View style={styles.item} key={this.props.index}>
+      /*<Animated.View
+          style={{transform: [{translateX: this.translateX}], height: 75, backgroundColor : 'red', opacity:0.8 }} {...this._panResponder.panHandlers}>
+        </Animated.View>*/
+        <View style={styles.item} key={this.props.index}{...this._panResponder.panHandlers}>
+            <Animated.View
+              style={{width: this.translateX, height: 75, backgroundColor : 'green',position : 'absolute', zIndex :5 }}
+              opacity={0.5}>
+            </Animated.View>
             <View style={styles.checkBox}>
             <CheckBox
 
@@ -42,14 +85,18 @@ export default class ListItem extends Component {
     )
     :
     (
-      <View style={styles.item} key={this.props.index}>
+      <View style={styles.item} key={this.props.index}{...this._panResponder.panHandlers}>
+      <Animated.View
+        style={{width: this.translateX, height: 75, backgroundColor : 'green',position : 'absolute', zIndex :5 }}
+        opacity={0.5} >
+      </Animated.View>
       <View style={styles.checkBox}>
         <CheckBox
         checked={this.props.check[this.props.index]}
         onPress={() => this.props.doCheck(this.props.index)}
         />
         </View>
-        <View style={styles.wText}>
+        <View style={styles.wText} >
         <TextInput ref={this.props.index} style = {styles.itemtext} value = {this.props.item}
                         onChangeText={(text) =>{this.props.type(this.props.index, text)}}
                         onEndEditing={() =>{this.props.submit(this.props.index)}}
@@ -69,6 +116,7 @@ export default class ListItem extends Component {
           <Image style={styles.addListFolderImage} source={require('../images/remove_button.png')} />
         </TouchableOpacity>
         </View>
+
       </View>
     )
   }
